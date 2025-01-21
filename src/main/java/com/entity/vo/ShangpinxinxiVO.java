b9087724d1a7292070bf411d2cfb201b1157e768
yulinyu <2508000341@QQ.COM>
2025-01-21 17:29:49 +0800

    1

---
+++ src/main/java/com/entity/vo/ShangpinxinxiVO.java
@@ -0,0 +1,249 @@
+package com.entity.vo;
+
+import com.entity.ShangpinxinxiEntity;
+
+import com.baomidou.mybatisplus.annotations.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.util.Date;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+ 
+
+/**
+ * 商品信息
+ * 手机端接口返回实体辅助类 
+ * （主要作用去除一些不必要的字段）
+ * @author 
+ * @email 
+ * @date 2022-03-14 17:37:35
+ */
+public class ShangpinxinxiVO  implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	 			
+	/**
+	 * 类型
+	 */
+	
+	private String leixing;
+		
+	/**
+	 * 商品图片
+	 */
+	
+	private String shangpintupian;
+		
+	/**
+	 * 商品规格
+	 */
+	
+	private String shangpinguige;
+		
+	/**
+	 * 商品简介
+	 */
+	
+	private String shangpinjianjie;
+		
+	/**
+	 * 商品详情
+	 */
+	
+	private String shangpinxiangqing;
+		
+	/**
+	 * 生产地
+	 */
+	
+	private String shengchandi;
+		
+	/**
+	 * 上架日期
+	 */
+		
+	@JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd HH:mm:ss")
+	@DateTimeFormat 
+	private Date shangjiariqi;
+		
+	/**
+	 * 最近点击时间
+	 */
+		
+	@JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd HH:mm:ss")
+	@DateTimeFormat 
+	private Date clicktime;
+		
+	/**
+	 * 点击次数
+	 */
+	
+	private Integer clicknum;
+		
+	/**
+	 * 价格
+	 */
+	
+	private Float price;
+				
+	
+	/**
+	 * 设置：类型
+	 */
+	 
+	public void setLeixing(String leixing) {
+		this.leixing = leixing;
+	}
+	
+	/**
+	 * 获取：类型
+	 */
+	public String getLeixing() {
+		return leixing;
+	}
+				
+	
+	/**
+	 * 设置：商品图片
+	 */
+	 
+	public void setShangpintupian(String shangpintupian) {
+		this.shangpintupian = shangpintupian;
+	}
+	
+	/**
+	 * 获取：商品图片
+	 */
+	public String getShangpintupian() {
+		return shangpintupian;
+	}
+				
+	
+	/**
+	 * 设置：商品规格
+	 */
+	 
+	public void setShangpinguige(String shangpinguige) {
+		this.shangpinguige = shangpinguige;
+	}
+	
+	/**
+	 * 获取：商品规格
+	 */
+	public String getShangpinguige() {
+		return shangpinguige;
+	}
+				
+	
+	/**
+	 * 设置：商品简介
+	 */
+	 
+	public void setShangpinjianjie(String shangpinjianjie) {
+		this.shangpinjianjie = shangpinjianjie;
+	}
+	
+	/**
+	 * 获取：商品简介
+	 */
+	public String getShangpinjianjie() {
+		return shangpinjianjie;
+	}
+				
+	
+	/**
+	 * 设置：商品详情
+	 */
+	 
+	public void setShangpinxiangqing(String shangpinxiangqing) {
+		this.shangpinxiangqing = shangpinxiangqing;
+	}
+	
+	/**
+	 * 获取：商品详情
+	 */
+	public String getShangpinxiangqing() {
+		return shangpinxiangqing;
+	}
+				
+	
+	/**
+	 * 设置：生产地
+	 */
+	 
+	public void setShengchandi(String shengchandi) {
+		this.shengchandi = shengchandi;
+	}
+	
+	/**
+	 * 获取：生产地
+	 */
+	public String getShengchandi() {
+		return shengchandi;
+	}
+				
+	
+	/**
+	 * 设置：上架日期
+	 */
+	 
+	public void setShangjiariqi(Date shangjiariqi) {
+		this.shangjiariqi = shangjiariqi;
+	}
+	
+	/**
+	 * 获取：上架日期
+	 */
+	public Date getShangjiariqi() {
+		return shangjiariqi;
+	}
+				
+	
+	/**
+	 * 设置：最近点击时间
+	 */
+	 
+	public void setClicktime(Date clicktime) {
+		this.clicktime = clicktime;
+	}
+	
+	/**
+	 * 获取：最近点击时间
+	 */
+	public Date getClicktime() {
+		return clicktime;
+	}
+				
+	
+	/**
+	 * 设置：点击次数
+	 */
+	 
+	public void setClicknum(Integer clicknum) {
+		this.clicknum = clicknum;
+	}
+	
+	/**
+	 * 获取：点击次数
+	 */
+	public Integer getClicknum() {
+		return clicknum;
+	}
+				
+	
+	/**
+	 * 设置：价格
+	 */
+	 
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+	
+	/**
+	 * 获取：价格
+	 */
+	public Float getPrice() {
+		return price;
+	}
+			
+}
---
+++ src/main/webapp/admin/src/utils/base.js
@@ -0,0 +1,16 @@
+const base = {
+    get() {
+        return {
+            url : "http://localhost:8080/ssm0fkb1/",
+            name: "ssm0fkb1",
+            // 退出到首页链接
+            indexUrl: 'http://localhost:8080/ssm0fkb1/front/index.html'
+        };
+    },
+    getProjectName(){
+        return {
+            projectName: "果蔬经营平台系统"
+        } 
+    }
+}
+export default base
